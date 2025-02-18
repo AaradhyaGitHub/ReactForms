f8469f6f1fe9a69f7795f17c26319894bd7994d9
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-02-17 21:14:42 -0800

    Explored 2 methods of input validation

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -4,28 +4,47 @@ export default function Login() {
   // const [enteredPassword, setEnteredPassword] = useState("");
 
   const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: ""
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  });
+
+  const emailIsInvalid =
+    didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Form Submitted!");
   }
 
-  function handleInputChange(identifier, value){
-    setEnteredValues(
-      prevValues => ({
-        ...prevValues, 
-        [identifier]: value
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value
+    }));
+    setDidEdit(
+      prevEdit => ({
+        ...prevEdit, 
+        [identifier]: false
       })
     )
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
+
   }
 
   function handleEmailChange(event) {
     setEnteredEmail(event.target.value);
   }
- 
 
   return (
     <form onSubmit={handleSubmit}>
@@ -38,9 +57,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            onChange={(event)=>handleInputChange('email', event.target.value)}
-            value = {enteredValues.email}
+            onBlur={() => handleInputBlur("email")}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please Enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -49,9 +72,10 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            oonChange={(event)=>handleInputChange('password', event.target.value)}
-            value = {enteredValues.password}
-            
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
           />
         </div>
       </div>
