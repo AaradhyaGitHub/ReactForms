b1daf34af02413cf2760490d9a4d822ef9fe06bb
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-02-17 17:19:51 -0800

    Individual State for individual user inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log('Form Submitted!')
+    console.log("Form Submitted!");
   }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+ 
 
   return (
     <form onSubmit={handleSubmit}>
@@ -12,18 +20,31 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value = {enteredEmail}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" onClick={handleSubmit}>Login</button>
+        <button className="button" onClick={handleSubmit}>
+          Login
+        </button>
       </p>
     </form>
   );
